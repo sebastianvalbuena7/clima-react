381277b65c979c506d45f376ee5f4996a4bd92be
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-03-15 23:01:43 -0500

    Proyeto Clima React Terminado

---
+++ src/components/AppClima.jsx
@@ -0,0 +1,19 @@
+import Formulario from "./Formulario"
+import Resultado from "./Resultado"
+import Spinner from "./Spinner"
+import useClima from "../hooks/useClima"
+
+const AppClima = () => {
+    const { resultado, cargando, noResultado } = useClima()
+
+    return (
+        <>
+            <main className="dos columnas">
+                <Formulario />
+                {cargando ? <Spinner/> : resultado?.name ? <Resultado/> : noResultado ? <p className="text-error">{noResultado}</p> : <p>El clima se va a mostrar aquí</p>}
+            </main>
+        </>
+    )
+}
+
+export default AppClima
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+    return (
+        <div className="loading">
+            <div className="spinner"></div>
+        </div>
+    )
+}
+
+export default Spinner
